80ee3519913417afcdba4a954c577386e11c69a2
SHINTA <shinta.0806@gmail.com>
2026-02-18 13:07:21 +0900

    プロジェクトファイルを追加。

---
+++ TestDependencyProperty/Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace TestDependencyProperty.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ TestDependencyProperty/Contracts/Services/IPageService.cs
@@ -0,0 +1,6 @@
+﻿namespace TestDependencyProperty.Contracts.Services;
+
+public interface IPageService
+{
+    Type GetPageType(string key);
+}
